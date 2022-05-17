aa7458fe8fee37da30b991cf657b54254d60c1ab
Rasmi Jati <jatirasmi95@gmail.com>
2022-05-17 16:21:38 +0545

    Features|#182206898|CRUD operation for product using jdbc --create operation

--- src/main/java/com/syntech/repository/ProductRepository.java
+++ src/main/java/com/syntech/repository/ProductRepository.java
@@ -6,9 +6,8 @@
 package com.syntech.repository;
 
 import com.syntech.model.Product;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  *
@@ -16,32 +15,31 @@ import java.sql.ResultSet;
  */
 public class ProductRepository extends AbstractRepository<Product> {
 
-    Connection con = connectDB();
-    PreparedStatement stmt;
-    ResultSet rs;
-    
-//    @Override
-//    public void create(Product p){
-//        try{
-//            String insert = "insert into product((name,product_description,unitid) values(?,?,?)";
-//            stmt = con.prepareStatement(insert);
-//            stmt.setString(1,p.getName());
-//            stmt.setString(2,p.getDescription());
-//        }catch(Exception e){
-//            System.out.println("Inserted Successfully!!!!");
-//        }
-//    }
-    
-    
     @Override
-    public void edit(Product u) {
-        super.findAll().stream()
-                .filter(n -> n.getId().equals(u.getId()))
-                .forEach((Product un) -> {
-                    un.setName(u.getName());
-                    un.setDescription(u.getDescription());
-                    un.setUnitid(u.getUnitid());
-                });
+    public void create(Product p) {
+        try {
+            String insert = "insert into product (name,product_description,unitid) values(?,?,?)";
+            PreparedStatement stmt = connectDB().prepareStatement(insert);
+            stmt.setString(1, p.getName());
+            stmt.setString(2, p.getDescription());
+            stmt.setLong(3, p.getUnitid().getId());
+            int i = stmt.executeUpdate();
+            System.out.println(i + " Inserted Successfully!!");
+
+        } catch (SQLException e) {
+//            e.printStackTrace();
+            System.out.println("Insertion Failed!!!!");
+        }
     }
 
+    @Override
+    public void edit(Product u) {
+//        super.findAll().stream()
+//                .filter(n -> n.getId().equals(u.getId()))
+//                .forEach((Product un) -> {
+//                    un.setName(u.getName());
+//                    un.setDescription(u.getDescription());
+//                    un.setUnitid(u.getUnitid());
+//                });
+    }
 }

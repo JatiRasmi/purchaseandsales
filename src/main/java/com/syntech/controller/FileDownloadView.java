98a03eef2e967ac8d7e52e792376fb4b66dbf950
Rasmi Jati <jatirasmi95@gmail.com>
2022-08-05 15:40:00 +0545

    Features|#182914258|download excel file format using input and output stream to read file from system

--- src/main/java/com/syntech/controller/FileDownloadView.java
+++ src/main/java/com/syntech/controller/FileDownloadView.java
@@ -6,11 +6,13 @@
 package com.syntech.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import org.apache.commons.io.FileUtils;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  *
@@ -20,17 +22,39 @@ import org.apache.commons.io.FileUtils;
 @RequestScoped
 public class FileDownloadView {
 
-
-    public void downloadFile() {
-        try {
-            FacesContext.getCurrentInstance().getExternalContext().setResponseHeader("Content-disposition", "Attachment;filename=product.xlxs");
-            FacesContext.getCurrentInstance().getExternalContext().setResponseContentType("application/xls");
-            FileUtils.copyFile(new File("/home/rasmi/NetBeansProjects/purchaseandsales/src/main/webapp/excelfile/product.xlsx"),
-                    FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream());
-
-            FacesContext.getCurrentInstance().responseComplete();
-        }catch(IOException e){
-            
+    public void downloadFile() throws IOException {
+        File file = new File("/home/rasmi/NetBeansProjects/purchaseandsales/src/main/webapp/excelfile/product.xlsx");
+        byte[] buf;
+        try (InputStream fis = new FileInputStream(file)) {
+            buf = new byte[1024];
+            int offset = 0;
+            int numRead = 0;
+            while ((offset < buf.length) && ((numRead = fis.read(buf, offset, buf.length - offset)) >= 0)) {
+                offset += numRead;
+            }
         }
+        HttpServletResponse response
+                = (HttpServletResponse) FacesContext.getCurrentInstance()
+                        .getExternalContext().getResponse();
+
+        response.setContentType("application/xls");
+        response.setHeader("Content-Disposition", "attachment;filename=product.xlxs");
+        response.getOutputStream().write(buf);
+        response.getOutputStream().flush();
+        response.getOutputStream().close();
+        FacesContext.getCurrentInstance().responseComplete();
     }
+
+//    public void downloadFile() {
+//        try {
+//            FacesContext.getCurrentInstance().getExternalContext().setResponseHeader("Content-disposition", "Attachment;filename=product.xlxs");
+//            FacesContext.getCurrentInstance().getExternalContext().setResponseContentType("application/xls");
+//            FileUtils.copyFile(new File("/home/rasmi/NetBeansProjects/purchaseandsales/src/main/webapp/excelfile/product.xlsx"),
+//                    FacesContext.getCurrentInstance().getExternalContext().getResponseOutputStream());
+//
+//            FacesContext.getCurrentInstance().responseComplete();
+//        }catch(IOException e){
+//            
+//        }
+//    }
 }
